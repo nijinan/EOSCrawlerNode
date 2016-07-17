8b20f917e28c6ae3b33188251d2a383c37e4b150
nijinan <nijinan@pku.edu.cn>
2016-07-17 13:21:23 +0800

    A

---
+++ NewEOSCrawlerNode/src/cn/edu/pku/EOSCN/crawler/CommitCrawler.java
@@ -0,0 +1,17 @@
+package cn.edu.pku.EOSCN.crawler;
+
+public class CommitCrawler extends Crawler {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Crawl() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
